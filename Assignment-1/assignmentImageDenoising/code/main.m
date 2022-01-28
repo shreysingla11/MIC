3193141427785cd6cb584d98569dcc6e633b250b
shreysingla11 <shreysingla2@gmail.com>
2022-01-29 01:47:17 +0530

    added loss function graph

--- Assignment-1/assignmentImageDenoising/code/main.m
+++ Assignment-1/assignmentImageDenoising/code/main.m
@@ -1,25 +1,26 @@
-% clf;
+clf;
 clear;
 images = load("../data/assignmentImageDenoisingPhantom.mat");
 noisy_im = images.imageNoisy;
 clear_im = images.imageNoiseless;
-% figure;
-% imshow(noisy_im);
-% figure;
-% imshow(clear_im);
 
 present = noisy_im;
 shape = size(noisy_im);
 h = shape(1);
 w = shape(2);
-alpha = 0.5;
-eta = 0.1;
-for k=1:10
+alpha = 0.8;
+eta = 0.01;
+iters = 100;
+Losses = zeros(iters,1);
+for k=1:iters
+    Losses(k) = loss(noisy_im, present);
     der = (1-alpha)*operate_der_like(noisy_im, present) + alpha*operate_der_prior(present);
     present = present - eta*der;
 end 
-% imshow(noisy_im);
-% figure;
-% imshow(present);
-% figure;
-% imshow(clear_im);
+imshow(noisy_im);
+figure;
+imshow(present);
+figure;
+imshow(clear_im);
+figure;
+plot(Losses);
