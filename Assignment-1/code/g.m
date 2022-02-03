22d7eca491f51cf6425f41a183a9c9f9256fe8b7
Jayesh Singla <jayesh11.3.01@gmail.com>
2022-02-03 16:06:45 +0530

    Speed up huber

--- Assignment-1/code/g.m
+++ Assignment-1/code/g.m
@@ -6,26 +6,16 @@ function [f,der] = g(im, gamma)
     global prior_type;
     
     if strcmp(prior_type,'huber')
-        ind_lin_pos = im > gamma;
-        ind_lin_neg = im < -gamma;
-        ind_lin = ind_lin_pos | ind_lin_neg;
-        ind_quad = abs(im) <= gamma;
-        
-        f = zeros(size(im));
-        der = zeros(size(im));
-    
-        f(ind_quad) = 0.5*(im(ind_quad).^2);
-        f(ind_lin) = gamma*abs(im(ind_lin)) - 0.5*gamma*gamma;
-        f = sum(f,'all');
-    
-        der(ind_quad) = im(ind_quad);
-        der(ind_lin_pos) = gamma;
-        der(ind_lin_neg) = -gamma;
+        mask_less = abs(im) <= gamma;
+        mask_more = abs(im) > gamma;
+        f = sum((0.5*im.^2).*mask_less + (gamma*abs(im) - 0.5*gamma*gamma).*mask_more,"all");
+        der = im.*mask_less + gamma*sign(im).*mask_more;
     elseif strcmp(prior_type,'gaussian')
         f = sum(im.^2,'all');
         der = 2*im;
     elseif strcmp(prior_type,'disadp')
         f = sum(gamma*abs(im) - gamma*gamma*log(1 + abs(im)/gamma),'all');
         der = gamma*sign(im) - gamma*gamma*(sign(im)./(gamma + abs(im)));
+    end
 
 end
